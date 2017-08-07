3349333980881216666bb8e53f89806f3359f12a
Valeriy Krashenko <vkrashenko@bk.ru>
2017-08-07 15:55:19 +0200

    refactor dev structure

--- vc-module-aviary-image-editor/vc-module-aviary-image-editor/Scripts/resources/imageEditorApi.js
+++
@@ -1,4 +0,0 @@
-﻿angular.module('virtoCommerce.aviaryImageEditorModule')
-.factory('imageEditorApi', ['$resource', function ($resource) {
-    return $resource('api/virtoCommerce.imageEditor');
-}]);
