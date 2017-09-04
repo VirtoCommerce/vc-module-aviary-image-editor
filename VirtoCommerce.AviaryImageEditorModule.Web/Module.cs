0642370f674d1679fdf56e0200c1a2996aec8173
Valeriy Krashenko <vkrashenko@bk.ru>
2017-09-04 15:43:37 +0200

    fix module error; up module version

--- VirtoCommerce.AviaryImageEditorModule.Web/Module.cs
+++ VirtoCommerce.AviaryImageEditorModule.Web/Module.cs
@@ -1,13 +1,8 @@
-﻿using Microsoft.Practices.Unity;
-using VirtoCommerce.Platform.Core.Modularity;
+﻿using VirtoCommerce.Platform.Core.Modularity;
 
 namespace VirtoCommerce.AviaryImageEditorModule.Web
 {
     public class Module : ModuleBase
     {
-        public Module(IUnityContainer container)
-        {
-            IUnityContainer _container = container;
-        }
     }
 }
